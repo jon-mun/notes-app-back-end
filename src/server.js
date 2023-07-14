50794c8170ba274e97e33d8fe317790ca199beb1
jon-mun <jonathan.edmund@gmail.com>
2023-07-14 23:20:12 +0700

    consumer program finished

--- src/server.js
+++ src/server.js
@@ -20,6 +20,7 @@ const collaborations = require('./api/collaborations');
 const CollaborationsService = require('./services/postgres/CollaborationsService');
 const CollaborationsValidator = require('./validator/collaborations');
 
+// eslint-disable-next-line no-underscore-dangle
 const _exports = require('./api/exports');
 const ProducerService = require('./services/rabbitmq/ProducerService');
 const ExportsValidator = require('./validator/exports');
